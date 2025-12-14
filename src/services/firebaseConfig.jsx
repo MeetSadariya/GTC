588d6aa54cbf32a96fecaf6e153604e8ea91f638
Meet Sadariya <114211420+MeetSadariya@users.noreply.github.com>
2025-12-14 20:39:25 +0530

    Clear Firebase config values for security
    Removed sensitive Firebase configuration values.

--- src/services/firebaseConfig.jsx
+++ src/services/firebaseConfig.jsx
@@ -4,12 +4,12 @@ import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: "ai-trip-generator-8dbab.firebaseapp.com",
-  projectId: "ai-trip-generator-8dbab",
-  storageBucket: "ai-trip-generator-8dbab.appspot.com",
-  messagingSenderId: "382106833985",
-  appId: "1:382106833985:web:26662de7a394b3abf262c0",
-  measurementId: "G-E2FR3LG1K3"
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 
